9a1e61a158188f1b043c5af2e5ca8b2e6c1b5ceb
sinhey <sinheyyy@gmail.com>
2024-03-20 14:26:05 +0900

    second commit - design and add score

--- src/App.js
+++ src/App.js
@@ -12,37 +12,152 @@ import Box from './component/Box';
 const choice = {
   rock: {
     name: "Rock",
-    img: "https://t3.ftcdn.net/jpg/04/42/06/24/360_F_442062431_Wc6nHEWjQuM0MU5K7gYWOFIYdEcKquTm.jpg"
+    img: "https://cdn-icons-png.flaticon.com/128/9496/9496176.png"
   },
   scissors: {
     name: "Scissors",
-    img: "https://media.istockphoto.com/id/1146263300/vector/scissors-vector-design-illustration-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=mwp4-CXaKtvIq9VLvJbFtt94KhWmBPXJC9fFgrcPi3k="
+    img: "https://cdn-icons-png.flaticon.com/128/9534/9534501.png"
   },
   paper: {
     name: "Paper",
-    img: "https://media.istockphoto.com/id/1354369215/vector/black-and-white-vector-illustration-of-a-childrens-activity-coloring-book-page-with-pictures.jpg?s=612x612&w=0&k=20&c=jNdKmA8WAuHi2p-83Mz0cZx7E-9_ZXc1zFYiFEl9jg8="
+    img: "https://cdn-icons-png.flaticon.com/128/12355/12355903.png"
   }
 }
 
 function App() {
-  const [userSelect, setUserSelect] = useState(null);
+  const [userSelect, setUserSelect] = useState(null);           // 사용자 선택을 위한 useState
+  const [computerSelect, setComputerSelect] = useState(null);   // 컴퓨터 선택을 위한 useState
+  const [result, setResult] = useState("");                     // 승패 결과
+  const [userScore, setUserScore] = useState(0);                // user 점수
+  const [comScore, setComScore] = useState(0);                   // com 점수
 
+  // user가 버튼을 클릭할 때,
   const play = (userChoice) => {
     console.log("선택됨 - ", userChoice);
     setUserSelect(choice[userChoice]);
-    
+
+    // computerChoice 랜덤하게 값 받아서 set
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+
+    // 승패 판단
+    setResult(judgement(choice[userChoice], computerChoice));
+
+    count(judgement(choice[userChoice], computerChoice));
+
+  }
+
+  // 승패 판단 - 누나 ver(삼항연산자로 더 깔끔해짐!)
+  const judgement = (user, computer) => {
+    //console.log("user",user, "com",computer);
+
+    if (user.name == computer.name) {
+      return "tie";
+    }
+    else if (user.name == "Rock")
+      return computer.name == "Scissors" ? "win" : "lose";
+    else if (user.name == "Scissors")
+      return computer.name == "Paper" ? "win" : "lose";
+    else if (user.name == "Paper")
+      return computer.name == "Rock" ? "win" : "lose";
+
+  }
+
+  // 승패 판단 - if로 경우 다 따지는 버전
+  const judgement2 = (user, computer) => {
+    console.log("user", user, "com", computer);
+
+    // 비긴 경우
+    if (user.name == computer.name) {
+      console.log("tie");
+    }
+
+    // user가 가위로 이긴 경우
+    if (user.name == "Scissors" && computer.name == "Paper") {
+      console.log("user win");
+    }
+    else if (user.name == "Scissors" && computer.name == "Rock") {
+      console.log("com win");
+    }
+
+    // user가 바위로 이긴 경우
+    if (user.name == "Rock" && computer.name == "Scissors") {
+      console.log("user win");
+    }
+    else if (user.name == "Rock" && computer.name == "Paper") {
+      console.log("com win");
+    }
+
+    // user가 보로 이긴 경우
+    if (user.name == "Paper" && computer.name == "Rock") {
+      console.log("user win");
+    }
+    else if (user.name == "Paper" && computer.name == "Scissors") {
+      console.log("com win");
+    }
+  }
+
+  // random하게 고른 값을 반환 - computerChoice가 될 예정
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);  // choice 객체를 배열화 - 객체의 key 값만 뽑아서 array로 바꿔줌
+    console.log("item array", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);  // 0~3 random 발생
+    let final = itemArray[randomItem];    // random 하게 rock/paper/scissors 중 고름
+    return choice[final];                 // 객체 반환
+  }
+
+  // 점수 계산
+  const count = (result) => {
+    // result에 따른 점수 계산
+    if (result == "win") {
+      setUserScore(userScore + 1);
+    }
+    else if (result == "lose") {
+      setComScore(comScore + 1);
+    }
+
+    //console.log("result", result,"user:", userScore, "com:", comScore);
+  }
+
+  // 점수 reset
+  const reset = () => {
+    setUserScore(0);
+    setComScore(0);
+
+    // 화면 reset 도전!
+    setUserSelect(null);
+    setComputerSelect(null);
+    setResult("");
   }
 
   return (
     <div>
-      <div className='main'>
-        <Box title="YOU" item={userSelect}/>
-        {/*<Box title="COMPUTER" />*/}
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Orbit&display=swap')
+      </style>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap')
+      </style>
+      <div className='title'>
+        <h1>가위! 바위! 보!</h1>
+      </div>
+      <div className='score'>
+        <h1>{userScore} : {comScore}</h1>
       </div>
       <div className='main'>
-        <button onClick={() => play("scissors")}>가위</button>
-        <button onClick={() => play("rock")}>바위</button>
-        <button onClick={() => play("paper")}>보</button>
+        <Box title="YOU" item={userSelect} result={result} />
+        <Box title="COMPUTER" item={computerSelect} result={result} />
+      </div>
+      <div className='main'>
+        <button className='btn' onClick={() => play("scissors")}>가위</button>
+        <button className='btn' onClick={() => play("rock")}>바위</button>
+        <button className='btn' onClick={() => play("paper")}>보</button>
+      </div>
+      <div className='main'>
+        <button className='btn2' onClick={() => reset()}>다시</button>
+      </div>
+      <div className='main'>
+        <h3>규칙 : 이기면 +1, 비기면 +0</h3>
       </div>
     </div>
   );
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,12 +1,32 @@
 import React from 'react'
 
 const Box = (props) => {
-    console.log("props", props)
+  console.log("props", props);
+  let result, result2;
+
+  // props.result가 빈 값이 아니라면
+  if (props.result != "") {
+    // tie일 경우는 그대로 tie
+    if (props.result == "tie") {
+      result = "tie";
+    }
+    // YOU는 props.result 그대로
+    else if (props.title == "YOU") {
+      result = props.result;
+    }
+    // COMPUTER는 반대로
+    else if (props.title == "COMPUTER") {
+      props.result == "win" ? result = "lose" : result = "win";
+    }
+
+    result2 = result.toUpperCase();
+  }
+
   return (
-    <div className='box'>
+    <div className={`box ${result}`}>
       <h1>{props.title}</h1>
-      <img className="item-img" src={props.item && props.item.img}/>
-      <h2>WIN</h2>
+      <img className="item-img" src={props.item && props.item.img} />
+      <h2>{result2}</h2>
     </div>
   )
 }
